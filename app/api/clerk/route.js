8b2bcfb022e414663d1de68974159d0615e42e06
Chandrahas10 <chandrahaskori9@gmail.com>
2026-05-30 09:25:00 +0530

    solve the db conn

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -1,47 +1,51 @@
 import { Webhook } from "svix";
 import connectDB from "@/config/Db";
-import User from "../../../models/User";
-import {headers} from "next/headers";
-import { NextRequest, NextResponse } from "next/dist/server/web/exports";
-import { timeStamp } from "node:console";
+import User from "@/models/User";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 
-export async function POST(req){
-    const wh =new Webhook(process.env.SIGNING_SECRET)
-    const headerPayload = await headers()
-    const svixHeaders ={
-        "svix-id":headerPayload.get("svix-id"),
-         "svix-timestamp":headerPayload.get("svix-timestamp"),
-        "svix-signature":headerPayload.get("svix-signature"),
-    };
-    // Get the payload and verify 
-    const payload=await req.json();
-    const body = JSON.stringify(payload);
-    const {data,type} =wh.verify(body,svixHeaders)
+export async function POST(req) {
+    const wh = new Webhook(process.env.SIGNING_SECRET);
 
-    // Prepare the user data to be saved in the database
-    const userData ={
-        _id:data.id,
-        email:data.email_addresses[0].email_addresses,
-        name:`${data.first_name} ${data.last_name}`,
-        image:data.image_url,
+    const headerPayload = await headers();
+
+    const svixHeaders = {
+        "svix-id": headerPayload.get("svix-id"),
+        "svix-timestamp": headerPayload.get("svix-timestamp"),
+        "svix-signature": headerPayload.get("svix-signature"),
     };
+
+    const body = await req.text();
+
+    const { data, type } = wh.verify(body, svixHeaders);
+
+    const userData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name || ""} ${data.last_name || ""}`,
+        image: data.image_url,
+    };
+
     await connectDB();
 
-    switch(type){
-        case 'user.created':
-            await User.create(userData)
+    switch (type) {
+        case "user.created":
+            await User.create(userData);
             break;
-        
-        case 'user.updated':
-            await User.findByIdAndUpdate(data.id,userData)
+
+        case "user.updated":
+            await User.findByIdAndUpdate(data.id, userData);
             break;
-        
-        case 'user.deleted':
-            await User.findByIdAndDelete(data.id)
+
+        case "user.deleted":
+            await User.findByIdAndDelete(data.id);
             break;
 
         default:
             break;
     }
-    return NextRequest.json({message:"Event received"})
+
+    return NextResponse.json({
+        success: true,
+    });
 }
